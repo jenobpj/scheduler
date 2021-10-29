7dd66bf154910dfba933b08e837afe7cb0351ac5
jenobpj <jenobgit@gmail.com>
2021-10-29 12:37:21 -0400

    display appointments if it is available or not

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,7 @@ import React from "react";
 import "components/Application.scss";
 import DayList from "components/DayList"
 import  { useState } from "react";
+import Appointment from "components/Appointment";
 const days = [
   {
     id: 1,
@@ -19,7 +20,44 @@ const days = [
     spots: 0,
   },
 ];
-
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
   return (
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,17 @@
-import React ,{Fragment}from "react";
+import React from "react";
 import "./Styles.scss"
-import "./Header"
-import "./Show"
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
+function showing(props){
+  return( props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> :<Empty/>)}
 
 export default function Appointment(props) {
+  console.log(props)
   return (<article className="appointment">
+   <Header time={props.time}/> 
 
+   {showing(props)}
   </article>);
 }
