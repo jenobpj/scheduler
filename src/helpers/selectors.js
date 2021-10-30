e028e0938382aaa60f3b43c408deb3287957ddc2
jenobpj <jenobgit@gmail.com>
2021-10-30 16:06:07 -0400

    add helper functon reterive the appointment of the day

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,4 +1,15 @@
 export const getAppointmentsForDay=(state,day)=>{
-  const dayObj=state.days.find(elem => elem.name === day)
-  return dayObj
+  const dayObj=state.days.find(elem => elem.name === day)//checking the days.name ===  day which we provide
+  if(!dayObj){
+    return[];
+  }
+  const appointmentIDs=dayObj.appointments;//it save the appointments of the day
+  const appointmentsForDay=[];//make new array
+ for(const id in state.appointments){//loop through the key in appointments
+   if(appointmentIDs.includes(Number(id))){//checks if the id includes in the appointments
+     appointmentsForDay.push(state.appointments[id]);//then push the whole object into new array
+   }
+ }
+ return appointmentsForDay;
+
 }
