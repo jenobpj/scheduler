25efb1b63985b6ca333dd1d6856d9d0a1ef83e02
jenobpj <jenobgit@gmail.com>
2021-10-31 22:47:26 -0400

    Fetch interviewers and store in state

--- src/components/Application.js
+++ src/components/Application.js
@@ -10,16 +10,18 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers:{}
   });
 const setDay = day => setState(prev => ({ ...prev, day }));
 
   useEffect(()=>{
    Promise.all([
      axios.get("/api/days"),
-     axios.get("/api/appointments")
+     axios.get("/api/appointments"),
+     axios.get("/api/interviewers")
    ]).then(all=>{
-     setState(prev =>({...prev,days:all[0].data,appointments:all[1].data}))
+     setState(prev =>({...prev,days:all[0].data,appointments:all[1].data,interviewers:all[2].data}));
    })
   },[])
   const appointmentsForDay=getAppointmentsForDay(state,state.day)
