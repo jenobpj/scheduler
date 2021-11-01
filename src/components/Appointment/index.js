43324380be171fc297aa714bf85d7faeb40d5c2e
jenobpj <jenobgit@gmail.com>
2021-11-01 16:34:42 -0400

    useVisulaMode in appointmentcomponent

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,12 +3,15 @@ import "./Styles.scss"
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "hooks/useVisualMode";
+const EMPTY="EMPTY";
+const SHOW ="SHOW";
 
 function showing(props){
   return( props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> :<Empty/>)}
   
 export default function Appointment(props) {
-  console.log(props)
+  
   return (<article className="appointment">
    <Header time={props.time}/> 
    {showing(props)}
