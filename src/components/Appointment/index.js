3743eef2bc75ae927a3ec04a1374b26d191477c1
jenobpj <jenobgit@gmail.com>
2021-11-01 17:15:59 -0400

    Use transition into create mode

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,9 +3,12 @@ import "./Styles.scss"
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
 import useVisualMode from "hooks/useVisualMode";
+
 const EMPTY="EMPTY";
 const SHOW ="SHOW";
+const CREATE="CREATE";
 
 
   
@@ -16,11 +19,20 @@ export default function Appointment(props) {
   return (
   <article className="appointment">
    <Header time={props.time}/> 
-   {mode === EMPTY && <Empty/>}
+   {mode === EMPTY && (
+        <Empty
+          onAdd={() => transition(CREATE)}
+        />
+      )}
       {mode === SHOW && (
         <Show
         student={props.interview.student}
         interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
         />
       )}
   </article>);
