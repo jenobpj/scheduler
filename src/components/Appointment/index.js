169f31d682ad0bcb7e8ba8cadf94ac110111c499
jenobpj <jenobgit@gmail.com>
2021-11-01 17:17:49 -0400

    add cancel

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -33,6 +33,7 @@ export default function Appointment(props) {
       {mode === CREATE && (
         <Form
           interviewers={[]}
+          onCancel={back}
         />
       )}
   </article>);
