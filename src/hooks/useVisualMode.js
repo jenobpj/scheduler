8e5758bce44b408e6d0a2c8bfe0736c508ab5b86
jenobpj <jenobgit@gmail.com>
2021-11-01 16:33:57 -0400

    build back limit validation for useVisulaMode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,17 +1,25 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  const transition = newMode => {
-    setMode(newMode);
-    setHistory(history => [...history, newMode]);
-  } 
-
+  const transition = (newMode, replace = false) => {    
+      setMode(newMode);
+         setHistory(history => {
+           if(replace){
+             
+          const newHistory = [...history];
+          newHistory.splice(- 1, 1, newMode);
+          return newHistory;
+           } else {
+             return [...history,newMode]
+           }
+          })
+        }
   const back = () => {
     setHistory(history => {
-      const newHistory = [...history].slice(0, -1);
+      const newHistory = history.length > 1 ? [...history].slice(0, -1) : [...history];
       setMode(newHistory[newHistory.length - 1]);
       return newHistory;
     })
