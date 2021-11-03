15cbec5abfc4981b0bf6ba3f7dce468c5cb77f18
jenobpj <jenobgit@gmail.com>
2021-11-03 12:25:23 -0400

    sue proptypes for validation in interviewlist

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
 import "components/InterviewerList.scss";
+import PropTypes from "prop-types"
 export default function InterviewerList(props) {
   const interviewListItems = props.interviewers.map((interview) => (
     <InterviewerListItem
@@ -19,3 +20,7 @@ export default function InterviewerList(props) {
     </section>
   );
 }
+InterviewerList.propTypes = {
+  value: PropTypes.number,
+  onChange: PropTypes.func.isRequired
+};
