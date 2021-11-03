8b50dfb9aac8077972152c98d723658c822f62cf
jenobpj <jenobgit@gmail.com>
2021-11-03 17:07:05 -0400

    E2e test for cancelling

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -33,4 +33,13 @@ it("should edit an interview", () => {
   cy.contains(".appointment__card--show", "Lydia Miller-Jones");
   cy.contains(".appointment__card--show", "Tori Malcolm");
 });
+
+it("should cancel an interview", () => {
+  cy.get("[alt=Delete]").click({ force: true });
+  cy.contains("Confirm").click();
+
+  cy.contains("Deleting").should("exist");
+  cy.contains("Deleting").should("not.exist");
+  cy.contains(".appointment__card--show", "Archie Cohen").should("not.exist");
+});
 });
